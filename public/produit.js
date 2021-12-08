471ddb4a94089fb69086015396b5a07a891ed0f3
Nina <morville5audrey@gmail.com>
2021-12-09 00:50:49 +0100

    dynamique loading page v1

---
+++ public/produit.js
@@ -0,0 +1,62 @@
+ 
+ 
+ 
+ 
+ //Appel à la fonction getQueryParam
+ getQueryParam();
+
+ //Déclaration de la fonction getQueryParam
+ function getQueryParam() {
+   //definition des query params pour manipuler les données de l'element au click
+   const urlSearchParam = new URLSearchParams(window.location.search);
+   const params = Object.fromEntries(urlSearchParam.entries());
+   console.log(params);
+
+
+   
+          //appelle à la fonction getApiElement qui définie les query strings et recupere un teddy par son id avec le .id qui fait reference au parametre dans l'url
+          getApiElement(params.id);
+
+
+           //Déclaration de la fonction permettant l'appel à l'api pour l'afichage des données d'un teddy en particulier
+            function getApiElement(photo) {
+
+                //appelle a l'api
+                fetch(`http://localhost:5600/api/${photo}`)
+
+                //Gestion de la promesse envoyé par l'api
+                .then((resp) => resp.json())
+
+                .then((data) => {
+                    //Affichage des éléments du Dom
+                    console.log(data);
+
+                    const box = document.getElementsByClassName('box');
+
+                    
+                    const domItemImg = document.createElement('img')
+                    const domItemName = document.createElement('div')
+
+                    domItemImg.src = data.imageUrl
+                    domItemImg.classList.add('img')
+                    domItemImg.width = 300
+                    domItemImg.height = 300
+                    domItemImg.setAttribute('alt', 'Teddy\'s picture')
+
+
+                    domItemName.textContent = data.name
+                    domItemName.classList.add('name')
+
+
+                    box.appendChild(domItemImg)
+                    box.appendChild(domItemName)
+                  
+                });
+            }
+ }
+        
+
+
+
+
+
